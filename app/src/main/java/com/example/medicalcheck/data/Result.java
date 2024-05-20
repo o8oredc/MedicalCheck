908329c0b5d2ade0ad7c50270cd6b7f932f432aa
Bailasan <o.bailasan.o@gmail.com>
2024-05-20 20:42:34 +0300

    V1.0 USB debugging worked

--- app/src/main/java/com/example/medicalcheck/data/Result.java
+++ app/src/main/java/com/example/medicalcheck/data/Result.java
@@ -3,7 +3,7 @@ package com.example.medicalcheck.data;
 /**
  * A generic class that holds a result success w/ data or an error exception.
  */
-public class Result<T> {
+public class    Result<T> {
     // hide the private constructor to limit subclass types (Success, Error)
     private Result() {
     }
